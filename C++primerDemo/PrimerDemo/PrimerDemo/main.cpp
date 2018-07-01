a06566ac72159b0f8155b4a22511a80053ecca83
WELCommand <ios_programming@163.com>
2018-07-01 18:30:30 +0800

    init project

---
+++ C++primerDemo/PrimerDemo/PrimerDemo/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  PrimerDemo
+//
+//  Created by WELCommand on 2018/7/1.
+//  Copyright © 2018年 WELCommand. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+
